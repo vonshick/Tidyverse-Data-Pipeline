1a3cdb2f7fad42f0c0afa97fb54ba7f2ac1641be
vonshick <kubawasik@vp.pl>
2020-08-12 09:45:37 +0200

    Update package

--- tests/testthat/test-test_of_column_names_in_data.R
+++ tests/testthat/test-test_of_column_names_in_data.R
@@ -2,7 +2,9 @@ test_that("Check if required columns are present in class dataset", {
   data_sets_list <- check_id_uniqueness()
 
   expect_true(
-    all(c("id", "name", "teaching_hours") %in% colnames(data_sets_list$class))
+    all(
+      c("id", "name", "teaching_hours") %in% colnames(data_sets_list$class)
+    )
   )
 })
 
@@ -10,6 +12,18 @@ test_that("Check if required columns are present in test_level dataset", {
   data_sets_list <- check_id_uniqueness()
 
   expect_true(
-    all(c("id", "displayName", "created_at") %in% colnames(data_sets_list$test_level))
+    all(
+      c("id", "displayName", "created_at") %in% colnames(data_sets_list$test_level)
+    )
+  )
+})
+
+test_that("Check if required columns are present in test dataset", {
+  data_sets_list <- check_id_uniqueness()
+
+  expect_true(
+    all(
+      c("id", "created_at", "authorized_at", "test_level_id", "overall_score") %in% colnames(data_sets_list$test)
+    )
   )
 })
